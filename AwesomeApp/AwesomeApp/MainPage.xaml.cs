66a2f2a5e4cfe3983ea1830bdb84b52586afa1eb
Jake Saterlay <jake@vocala.co>
2021-04-04 21:31:55 +0100

    Add project files.

---
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AwesomeApp.Utility;
+
+namespace AwesomeApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+
+        int noClicks = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnImageNameTapped(object sender, EventArgs args)
+        {
+            try
+            {
+
+                AudioUtil.PlayAudio("ohyeah.wav");
+
+                noClicks++;
+                lblCount.Text = noClicks.ToString();
+                if(noClicks == 10)
+                {
+                    Image.Source = "doublechoccookie";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ AwesomeApp/AwesomeApp/Utility/AudioUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace AwesomeApp.Utility
+{
+    public static class AudioUtil
+    {
+        public static void PlayAudio(string fileName)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            Stream audioStream = assembly.GetManifestResourceStream("AwesomeApp.Audio." + fileName);
+            var audio = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+            audio.Load(audioStream);
+            audio.Play();
+        }
+    }
+}
